0dd0beee7444b53870dfba0261757e6031d206c0
Dominik Schöni <dominik@dominikschoeni.ch>
2018-12-21 14:57:06 +0100

    fix(update): changing way of converting object

--- src/crud/update.ts
+++ src/crud/update.ts
@@ -1,7 +1,7 @@
 import { Route, HTTPMethod } from '../route'
 import { BaseEntity, FindOneOptions } from 'typeorm'
 import { Request, Response, NextFunction } from 'express'
-import { classToPlain, plainToClassFromExist } from 'class-transformer'
+import { classToPlain, plainToClass } from 'class-transformer'
 
 export class UpdateRoute extends Route {
   constructor(private model: typeof BaseEntity, path: string) {
@@ -13,7 +13,7 @@ export class UpdateRoute extends Route {
 
     // mandatory query-filter
     if (this.queryFilter) {
-      Object.assign(query, { where: this.queryFilter(request) })
+      Object.assign(query, { where: this.queryFilter(request), relations: this.relations })
     }
 
     const entity = await this.model.findOne(request.params.id, query)
@@ -22,17 +22,20 @@ export class UpdateRoute extends Route {
       return response.sendStatus(404)
     }
 
-    // copy over all supplied params to entity
-    const updatedEntity = plainToClassFromExist(entity, request.body)
+    // convert request to class
+    const newClass = (plainToClass(this.model, request.body) as unknown) as BaseEntity
 
     // validate object
-    const errors = await this.validateEntity(updatedEntity)
+    const errors = await this.validateEntity(newClass)
     if (errors && errors.length > 0) {
       return response.status(400).json(errors)
     }
 
+    // copy over all supplied params to entity
+    Object.assign(entity, newClass)
+
     // save object if valid
-    const savedEntity = await updatedEntity.save()
+    const savedEntity = await entity.save()
 
     return response.status(200).json(classToPlain(savedEntity))
   }
