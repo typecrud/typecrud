dd8781f47645b658ed921967703e74cda6aa9757
Dominik Schöni <dominik@dominikschoeni.ch>
2019-05-02 11:57:27 +0200

    fix(hooks): fixing async behavior

--- src/route.ts
+++ src/route.ts
@@ -55,19 +55,19 @@ export abstract class Route<T> {
 
   protected async preEntityHook(request: Request, entity: T[]) {
     if (this.hooks && this.hooks.pre && this.hooks.pre[this.crudType]) {
-      this.hooks.pre[this.crudType]!(request, entity)
+      await this.hooks.pre[this.crudType]!(request, entity)
     }
   }
 
   protected async postEntityHook(request: Request, entity: T[]) {
     if (this.hooks && this.hooks.post && this.hooks.post[this.crudType]) {
-      this.hooks.post[this.crudType]!(request, entity)
+      await this.hooks.post[this.crudType]!(request, entity)
     }
   }
 
   protected async postSerializationHook(request: Request, serializedObject: Object[]) {
     if (this.hooks && this.hooks.postSerialization && this.hooks.postSerialization[this.crudType]) {
-      this.hooks.postSerialization[this.crudType]!(request, serializedObject)
+      await this.hooks.postSerialization[this.crudType]!(request, serializedObject)
     }
   }
 
